e67ffef29f172d1795aac1153a85b66755ee024b
Alexander Titov <titov.cz@gmail.com>
2015-08-18 16:19:10 +0200

    Replaced intendation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ActiveRecord::Base
 
-	validates :title, presence: true
-	validates :date, presence: true
-	validates :description, presence: true
-	validates :place, presence: true
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :description, presence: true
+  validates :place, presence: true
 
-	default_scope { order('created_at DESC') }
+  default_scope { order('created_at DESC') }
 
 end
